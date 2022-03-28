060f69f24ee7aaa1a15c59a878e5a700d82bcab0
Oskar Łukasiewicz <lukasiewiczoskar079978@gmail.com>
2022-03-28 22:21:44 +0200

    Add tagclickhandler

--- js/script.js
+++ js/script.js
@@ -28,16 +28,17 @@
 
 	const optArticleSelector = '.post',
 		optTitleSelector = '.post-title',
-		optTitleListSelector = '.titles'
+		optTitleListSelector = '.titles',
+		optArticleTagsSelector = '.post-tags .list'
 
-	function generateTitleLinks() {
+	const generateTitleLinks = function(customSelector = '') {
 		/* remove contents of titleList */
 		const titleList = document.querySelector(optTitleListSelector)
 		titleList.innerHTML = ''
 
 		/* for each article */
-		const articles = document.querySelectorAll(optArticleSelector)
-
+		const articles = document.querySelectorAll(optArticleSelector + customSelector)
+        
 		let html = ''
 
 		for (let article of articles) {
@@ -58,10 +59,83 @@
 		for (let link of links) {
 			link.addEventListener('click', titleClickHandler)
 		}
-
 	}
-    generateTitleLinks()
+	generateTitleLinks()
+
+	const generateTags = function() {
+		/*[DONE] find all articles */
+		const articles = document.querySelectorAll(optArticleSelector)
+		/*[DONE] START LOOP: for every article: */
+		for (let article of articles) {
+			/*[DONE] find tags wrapper */
+			const tagsWrapper = article.querySelector(optArticleTagsSelector)
+			/*[DONE] make html variable with empty string */
+			let html = ''
+			/*[DONE] get tags from data-tags attribute */
+			const articleTags = article.getAttribute('data-tags')
+			/*[DONE] split tags into array */
+			const articleTagsArray = articleTags.split(' ')
+			/*[DONE] START LOOP: for each tag */
+			for (let tag of articleTagsArray) {
+				/*[DONE] generate HTML of the link */
+				const linkHtml = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>'
+				/*[DONE] add generated code to html variable */
+				html = html + linkHtml
+				/*[DONE] END LOOP: for each tag */
+			}
+			/*[DONE] insert HTML of all the links into the tags wrapper */
+            tagsWrapper.innerHTML = html
+		    /*[DONE] END LOOP: for every article: */
+		}
+	}
+
+	generateTags()
+
+    function tagClickHandler(event){
+        /*[DONE] prevent default action for this event */
+        event.preventDefault()
+        /*[DONE] make new constant named "clickedElement" and give it the value of "this" */
+        const clickedElement = this
+        /*[DONE] make a new constant "href" and read the attribute "href" of the clicked element */
+        const href = clickedElement.getAttribute('href')
+        /*[DONE] make a new constant "tag" and extract tag from the "href" constant */
+        const tag = href.replace('#tag-', '')
+        /*[DONE] find all tag links with class active */
+        const tagLinks = document.querySelectorAll('a.active[href^="#tag-"]')
+        /*[DONE] START LOOP: for each active tag link */
+        for (let tagLink of tagLinks){
+
+          /*[DONE] remove class active */
+            tagLink.classList.remove('active')
+        /*[DONE] END LOOP: for each active tag link */
+        }     
+        /*[DONE] find all tag links with "href" attribute equal to the "href" constant */
+        const hrefTagLinks = document.querySelectorAll('a[href="' + href + '"]')
+        /*[DONE] START LOOP: for each found tag link */
+        for (let hrefTagLink of hrefTagLinks){
+          /*{DONE} add class active */
+            hrefTagLink.classList.add('active')
+        /*{DONE} END LOOP: for each found tag link */
+        }
+        /*{DONE} execute function "generateTitleLinks" with article selector as argument */
+        generateTitleLinks('[data-tags~="' + tag + '"]')
+      }
+      
+      function addClickListenersToTags(){
+        /* find all links to tags */
+        const tagLinks = document.querySelectorAll('a[href^="#tag-"]')
+        /* START LOOP: for each link */
+        for (let tagLink of tagLinks){
+          /* add tagClickHandler as event listener for that link */
+            tagLink.addEventListener('click', tagClickHandler)
+        /* END LOOP: for each link */
+        }
+      }
+      
+      addClickListenersToTags();
+
+
+      
 
     
-
 }
